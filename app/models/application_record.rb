b6156f28ae68c1092bf8f4692d8973700acd6b79
Ubong Nehemiah <ubongnehemiah@gmail.com>
2023-03-16 12:31:24 +0100

    render and paginate users index view
    - updated root to users index
    - used gem 'pagy' for pagination
    - added support for search and filtering coming soon

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,17 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def self.order_by(records, params = {})
+    sort_by = JSON.parse(params['sort_by']) if params['sort_by'].present?
+    if sort_by.present?
+      sort_by.each do |order_hash|
+        order_key = order_hash['id'].to_sym
+        order_in = order_hash['desc'] ? 'desc' : 'asc'
+        records = records.order("#{order_attr[order_key]} #{order_in}")
+      end
+    elsif records.respond_to? :ordered
+      records = records.ordered
+    end
+    records
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  def self.search(params)
+    records = User.all
+    records = records.where('gender ILIKE ?', "#{params[:gender]}%") if params[:gender].present?
+    records = records.where('height ILIKE ?', "#{params[:height]}%") if params[:height].present?
+    if params[:name].present?
+      records = records.where('"firstName" ILIKE ? OR "lastName" ILIKE ?', "#{params[:name]}%",
+                              "#{params[:name]}%")
+    end
+    records = records.where('email ILIKE ?', "#{params[:email]}%") if params[:email].present?
+    order_by(records, params)
+  end
 end
