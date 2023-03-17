a2e94d756b0c61f8d7c28eb48ade0c974cff8ae8
Ubong Nehemiah <ubongnehemiah@gmail.com>
2023-03-17 03:50:22 +0100

    add filter by gender and height
    - updated user name and email search
    - added turbo confirm for bulk delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,25 @@ class User < ApplicationRecord
 
   def self.search(params)
     records = User.all
-    records = records.where('gender ILIKE ?', "#{params[:gender]}%") if params[:gender].present?
-    records = records.where('height ILIKE ?', "#{params[:height]}%") if params[:height].present?
+    records = records.where('gender ILIKE ?', "%#{params[:gender]}%") if params[:gender].present?
+    if params[:height].present?
+      records = if params[:height] == '>'
+                  records.where('height > ?', 170)
+                else
+                  records.where('height < ?', 170)
+                end
+    end
     if params[:name].present?
       records = records.where('"firstName" ILIKE ? OR "lastName" ILIKE ?', "#{params[:name]}%",
-                              "#{params[:name]}%")
+                              "%#{params[:name]}%")
     end
-    records = records.where('email ILIKE ?', "#{params[:email]}%") if params[:email].present?
+    records = records.where('email ILIKE ?', "%#{params[:email]}%") if params[:email].present?
     order_by(records, params)
   end
 
   def send_removed_mail
     job_params = {}
     job_params['email'] = email
-    UserMailerJob.perform_in(30.minutes, job_params)
+    UserMailerJob.perform_in(10.seconds, job_params)
   end
 end
